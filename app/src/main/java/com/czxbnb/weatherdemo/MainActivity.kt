36437e9cf6708673815d107f221d5d98495018b0
Zixin Cheng <zixin.cheng@news.com.au>
2022-04-20 01:10:51 +1000

    first commit

---
+++ app/src/main/java/com/czxbnb/weatherdemo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.czxbnb.weatherdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.czxbnb.weatherdemo.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
